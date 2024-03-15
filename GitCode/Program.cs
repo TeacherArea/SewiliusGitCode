c7fc47001fc0edbb33f77157fe333f0c08f568d8
TeacherArea <sewilius.berg@initialize.se>
2024-03-15 10:34:21 +0100

    First commit: Primary code

---
+++ GitCode/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GitCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Samarbetsappen");
+            int run = 0;
+            while (true)
+            {
+                Console.WriteLine("Välj i menyn (ett heltal)");
+                run = int.Parse(Console.ReadLine());
+                if (run == 1)
+                {
+                    Greetings();
+                }
+                else if (run == 2)
+                {
+                    Sewilius();
+                }
+                else
+                {
+                    Console.WriteLine("Hej då");
+                    break;
+                }
+            }
+        }
+        static void Greetings()
+        {
+            Console.WriteLine("Hello from GitCode");
+        }
+        static void Sewilius()
+        {
+            Console.WriteLine("Hello from Sewilius");
+        }
+    }
+}
