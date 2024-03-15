c318ec5d02b36c7f94f25c769ed1620d0a66324e
Edvin Nyholm <edvin.nyholm@learnet.se>
2024-03-15 10:30:36 +0100

    First Commit: Primary code

---
+++ GitCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GitCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen!");
+
+            int num = 0;
+           while (true)
+            {
+                Console.WriteLine("Välj i menyn (ett heltal)");
+                num = int .Parse(Console.ReadLine());
+                if (num == 1) 
+                { 
+                    Greetings();
+                }
+
+                else if (num == 2)
+                {
+                    Edvin();
+                }
+            }
+        }
+
+        static void Greetings()
+        {
+            Console.WriteLine("Tjo tjo från GitCode");
+            
+        }
+        static void Edvin()
+        {
+               Console.WriteLine("Edvin");
+        }
+    }
+}
