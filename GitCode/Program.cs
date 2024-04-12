d42cc4d3d04cb291f59e2ce9973b2c525e5418f9
TeacherArea <sewilius.berg@initialize.se>
2024-04-12 09:56:52 +0200

    ändrade i Counter()

--- GitCode/Program.cs
+++ GitCode/Program.cs
@@ -34,7 +34,7 @@
             for (int i = 0; i < 10; i++) {
                 Console.WriteLine(i);
             }
-            
+            Console.Write("Slut på uppgräkningen");
         }
     }
 }
