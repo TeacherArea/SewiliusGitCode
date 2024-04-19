dd64cda7884cbbe5e212eb2a860c09a19f419d0c
Viktor1009 <viktorosterberg10@gmail.com>
2024-04-19 10:02:45 +0200

    viktor tested to commit

--- GitCode/obj/Debug/net8.0/GitCode.AssemblyInfo.cs
+++ GitCode/obj/Debug/net8.0/GitCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac2e1a6e6f65a355f0a1d162712447c9fa607a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01002ce96ef7f40d8fc4a477966faa1e15fd820e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
