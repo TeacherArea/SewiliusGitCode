322c272e17c8f4e7dfbb4fc9c6de59575ff5dec4
Christopher Falk <familjenfalklive@gmail.com>
2024-04-19 10:21:36 +0200

    changed code to a better one

--- GitCode/Program.cs
+++ GitCode/Program.cs
@@ -4,36 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till Samarbetsappen");
-            int run = 0;
-            while (true)
-            {
-                Console.WriteLine("Välj i menyn (ett heltal)");
-                run = int.Parse(Console.ReadLine());
-                if (run == 1)
-                {
-                    Greetings();
-                }
-                else if (run == 2)
-                {
-                    Counter();
-                }
-                else
-                {
-                    Console.WriteLine("Hej då");
-                    break;
-                }
-            }
-        }
-        static void Greetings()
-        {
-            Console.WriteLine("Hello from GitCode");
-        }
-        static void Counter()
-        {
-            for (int i = 0; i < 10; i++) {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine("Hello World");
+   
             
         }
     }
